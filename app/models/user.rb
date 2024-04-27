bceb0b5fedea5e616f7b042eb3bb110bf6517cb3
Daichi Ito <daichi.sjtf@gmail.com>
2024-04-27 12:03:56 +0900

    ゲストログイン機能を追加
    ゲストログイン機能を追加、アカウント情報を変更できないよう設定。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   mount_uploader :image, ImgUploader
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+
   get 'users' => 'users#dummy'
+  
   resources:users
 end
