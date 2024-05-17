296aff941716f549aa65a8433c96e7da136b6a5b
Daichi Ito <daichi.sjtf@gmail.com>
2024-05-17 19:45:12 +0900

    Favoriteモデルの作成
    Favoriteモデルを作成、User・OnsenモデルにFavoriteモデルとの１対多の関係を定義

--- app/models/onsen.rb
+++ app/models/onsen.rb
@@ -1,5 +1,6 @@
 class Onsen < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :onsen_name, presence: true
   validates :onsen_introduction, presence: true
