838d61434f8408455e6929d97466b14231a00149
Daichi Ito <daichi.sjtf@gmail.com>
2024-05-21 23:11:27 +0900

    precompileの解消のため、アセットパイプラインを整える

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module HotSpringApp
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
   end
 end
