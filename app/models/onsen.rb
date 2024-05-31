231edc9aa50990f69b998bcc85d21d80267e20d4
Daichi Ito <daichi.sjtf@gmail.com>
2024-05-31 18:45:31 +0900

    人気順並べ替え機能の実装
    温泉一覧をいいねが多い順に並べ替える機能を実装、一覧画面に更新順・人気順に並べ替えるボタンを作成

--- app/models/onsen.rb
+++ app/models/onsen.rb
@@ -1,6 +1,7 @@
 class Onsen < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user 
 
   validates :onsen_name, presence: true, length: {maximum: 50}
   validates :onsen_introduction, presence: true, length: {in: 5..300}
